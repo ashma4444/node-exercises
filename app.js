9f49dab99763d7ca5827175d9e2a444347b764ab
ashma4444 <ashma20570218@gmail.com>
2023-08-17 10:10:17 +0545

    Learned about npm qrcode package

---
+++ app.js
@@ -0,0 +1,25 @@
+const http = require("http");
+var QRCode = require("qrcode");
+
+// render a qrcode for the terminal -> toString
+// Generate QR Code for  the url on the terminal using package qrcode
+QRCode.toString("facebook.com", function (err, url) {
+  console.log(url);
+});
+
+http
+  .createServer(async (req, res) => {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1> Welcome! </h1>");
+
+    // to display qrcode in image format in browser -> toDataURL
+    const urlData = await QRCode.toDataURL("www.facebook.com");
+    res.write(`<img src="${urlData}"> </img>`);
+    res.end();
+  })
+  .listen(3001);
+
+console.log("App is running in port 3001");
+
+// to run: node app.js
+// browser ma run garna: localhost:3001
