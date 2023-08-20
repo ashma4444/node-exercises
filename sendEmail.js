5abac978523e1515309cdb665fd3f85b7448f2f5
ashma4444 <ashma20570218@gmail.com>
2023-08-20 14:01:46 +0545

    learned email send using nodemailer

---
+++ sendEmail.js
@@ -0,0 +1,35 @@
+"use strict";
+const nodemailer = require("nodemailer");
+
+require("dotenv").config();
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    // TODO: replace `user` and `pass` values from <https://forwardemail.net>
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD,
+  },
+});
+
+// async..await is not allowed in global scope, must use a wrapper
+async function main() {
+  // send mail with defined transport object
+  const info = await transporter.sendMail({
+    from: "np01cp4s210041@islingtoncollege.edu.np", // sender address
+    to: "ashma20570218@gmail.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world! Mail sent successfully.", // plain text body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  //
+  // NOTE: You can go to https://forwardemail.net/my-account/emails to see your email delivery status and preview
+  //       Or you can use the "preview-email" npm package to preview emails locally in browsers and iOS Simulator
+  //       <https://github.com/forwardemail/preview-email>
+  //
+}
+
+main().catch(console.error);
